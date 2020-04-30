e2795a1ecee706e866d6e9c47e8f57bdbc71d021
seungwooKim99 <sonicdx886@gmail.com>
2020-04-30 18:08:20 +0900

    2020.04.30#Header

--- src/api/User/unlockUser/unlockUser.js
+++ src/api/User/unlockUser/unlockUser.js
@@ -1,20 +1,24 @@
 import { prisma } from "../../../../generated/prisma-client";
 
 export default {
-    try {
-        await prisma.updateUser({
-            where: {
-                email
-            },
-            data: {
-                isUnlocked: true
+    Mutation: {
+        unlockUser: async (_, args) => {
+            const { email } = args;
+            const user = await prisma.user({ email });
+            try {
+                await prisma.updateUser({
+                    where: {
+                        email
+                    },
+                    data: {
+                        isUnlocked: true
+                    }
+                });
+                return true;
             }
-        });
-        return true;
-    }
             catch{
-        return false;
-    }
-}
+                return false;
+            }
+        }
     }
 }
